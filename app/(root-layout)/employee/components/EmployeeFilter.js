d17c8b657feb8409dd50cfddfe35ba367383301f
csenazmul96 <csenazmul96@gmail.com>
2025-07-22 01:01:14 +0600

    Add EmployeeFilter component to employee page
    Introduce a new `EmployeeFilter` component and integrate it into the employee page. This sets up the structure for future enhancements involving employee filtering functionality.

---
+++ app/(root-layout)/employee/components/EmployeeFilter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function EmployeeFilter(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default EmployeeFilter;
--- app/(root-layout)/employee/page.js
+++ app/(root-layout)/employee/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import LmsPageHeading from "@/components/common/LmsPageHeading";
 import {getEmployees} from "@/utils/api/admin/EmployeeAPI";
 import EmployeeDataTable from "@/app/(root-layout)/employee/components/EmployeeDataTable";
+import EmployeeFilter from "@/app/(root-layout)/employee/components/EmployeeFilter";
 
 export default async function page({searchParams}) {
     const queryParams = await searchParams;
@@ -11,7 +12,7 @@ export default async function page({searchParams}) {
     return (
         <div className="flex flex-col member-list">
             <LmsPageHeading title="Employee List" tooltip={'Employee List'}/>
-
+<EmployeeFilter />
             <div className="member-list-table pt-16">
                 <EmployeeDataTable employees={employees} pagination={pagination} queryParams={queryParams}/>
             </div>
