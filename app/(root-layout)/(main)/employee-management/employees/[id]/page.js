f11c507cc09f3a3df3b8e1fc8368640bb63e4d6a
csenazmul96 <csenazmul96@gmail.com>
2025-08-03 11:54:40 +0600

    Update employee form validation and improve UI responsiveness

--- app/(root-layout)/(main)/employee-management/employees/[id]/page.js
+++ app/(root-layout)/(main)/employee-management/employees/[id]/page.js
@@ -1,7 +1,33 @@
-export default async function Page() {
+import LmsPageHeading from "@/components/common/LmsPageHeading";
+import {getsSingleEmployee} from "@/utils/api/employeeApi";
+import FieldWrapper from "@/components/common/form/FieldWrapper";
+import {Eye, EyeOff} from "lucide-react";
+import {Button} from "@/components/common/button";
+import React from "react";
+
+export default async function Page({params}) {
+    const {id} = params;
+    const employee = await getsSingleEmployee(id);
     return (
-        <>
-            test
-        </>
+        <div className="flex flex-col member-registration">
+            <LmsPageHeading title="Employee Details"/>
+            {employee && (
+                <div className="registration-form">
+                    <FieldWrapper label="Status">
+                        <Button
+                            color={`${employee.status ? 'primaryNoBgRoundedSmall' : "secondaryLightRoundedSmall"}`}>
+                            {employee.status ?
+                                <> <Eye size={16} /> Active</>
+                                :
+                                <> <EyeOff size={16} /> Inactive</>
+                            }
+                        </Button>
+                    </FieldWrapper>
+                    <FieldWrapper label="Name">
+                        <span>{employee.first_name} {employee.last_name}</span>
+                    </FieldWrapper>
+                </div>
+            )}
+        </div>
     );
 }
--- utils/api/employeeApi.js
+++ utils/api/employeeApi.js
@@ -14,6 +14,16 @@ export async function getsEmployee (params) {
         throw new Error(JSON.stringify(res));
     }
 }
+export async function getsSingleEmployee (id) {
+    const req = await fetchRequest(`/employees/${id}`)
+    const res = await req.json();
+
+    if (req.ok) {
+        return res.data
+    } else {
+        throw new Error(JSON.stringify(res));
+    }
+}
 
 export async function checkIfEmployeeExists (payload) {
     try {
