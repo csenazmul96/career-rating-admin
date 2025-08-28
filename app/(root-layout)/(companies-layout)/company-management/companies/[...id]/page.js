711c6d43e4336d87610e5c28981d3c6a7176c745
csenazmul96 <csenazmul96@gmail.com>
2025-08-28 22:46:00 +0600

    Refactor company management page and add table/filter.
    Updated the company management page to include `CompaniesFilter` and `CompaniesDataTable` components for improved functionality. Adjusted API call parameters to use industry-specific keys and updated the heading to "Companies".

--- app/(root-layout)/(companies-layout)/company-management/companies/[...id]/page.js
+++ app/(root-layout)/(companies-layout)/company-management/companies/[...id]/page.js
@@ -1,5 +1,9 @@
 import LmsPageHeading from "@/components/common/LmsPageHeading";
 import {getCompanies} from "@/utils/api/career/companiesAPI";
+import CompaniesFilter
+    from "@/app/(root-layout)/(companies-layout)/company-management/companies/components/CompaniesFilter";
+import CompaniesDataTable
+    from "@/app/(root-layout)/(companies-layout)/company-management/companies/components/CompaniesDataTable";
 
 export default async function Page({params, searchParams}) {
     const searchParamsAll = await searchParams
@@ -8,14 +12,14 @@ export default async function Page({params, searchParams}) {
     const secondId = id && id.length > 1 ? id[1] : ''
     const thirdId = id && id.length > 2 ? id[2] : ''
 
-    const {companies, pagination} = await getCompanies({...searchParamsAll, parentId, secondId, thirdId})
+    const {companies, pagination} = await getCompanies({...searchParamsAll, industry_id:parentId, sub_industry_id:secondId, sub_sub_industry_id:thirdId})
     return (
         <>
-            <>
-                <LmsPageHeading title="과정 관리" />
-                <div className="flex flex-col">
-                </div>
-            </>
+            <LmsPageHeading title="Companies" />
+            <CompaniesFilter queryParams={searchParamsAll}/>
+            <div className="flex flex-col pt-12 lg:pt-16">
+                <CompaniesDataTable companies={companies} pagination={pagination}/>
+            </div>
         </>
     );
 }
